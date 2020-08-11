42246bb89fd5b4e6354b4215585ff7a6567d902a
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-11 22:35:35 +0200

    add controller views and new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_202036) do
+ActiveRecord::Schema.define(version: 2020_08_11_203309) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_202036) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture_url"
   end
 
 end
